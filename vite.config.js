b043401f10bbc7e3334426655233f960a8daaf9b
Master-2290 <jordylubini64@gmail.com>
2026-01-16 15:47:37 +0100

    Fix: Remove __dirname from vite config for CI compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
     base: '/hopecomms/',
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                services: resolve(__dirname, 'services.html'),
-                realisations: resolve(__dirname, 'realisations.html'),
-                about: resolve(__dirname, 'about.html'),
-                contact: resolve(__dirname, 'contact.html'),
+                main: 'index.html',
+                services: 'services.html',
+                realisations: 'realisations.html',
+                about: 'about.html',
+                contact: 'contact.html',
             },
         },
     },
