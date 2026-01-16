e509485fc980a8ccf738fb7c0ca1664da497efdb
Master-2290 <jordylubini64@gmail.com>
2026-01-16 15:43:04 +0100

    Fix: Configure multi-page build and move static scripts to public

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     base: '/hopecomms/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                services: resolve(__dirname, 'services.html'),
+                realisations: resolve(__dirname, 'realisations.html'),
+                about: resolve(__dirname, 'about.html'),
+                contact: resolve(__dirname, 'contact.html'),
+            },
+        },
+    },
 })
